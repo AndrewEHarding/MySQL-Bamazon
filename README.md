f9fc6830768f9ca0951c0ea03d7581fbe534e300
Andrew Harding <dreweharding@gmail.com>
2018-09-11 01:43:10 -0400

    list inventory

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 
 ### **Setup**
 1. Place the **MySQL_Bamazon** folder into your virtual machine and `vagrant up`
-2. `cd` into the **MySQL-Bamazon** folder and run `npm install`
-3. PROFIT!
+2. Run `vagrant ssh`, then `cd/var/code`
+3. `cd` into the **MySQL-Bamazon** folder and run `npm install`
+4. Start the application with `node bamazonCustomer.js`
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,32 @@
+var cLog = console.log;
+
 var inquirer = require('inquirer');
 var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "SuperSecretPasswordHere",
+  database: "bamazon"
+});
+
+function fullInventory() {
+  cLog(`WELCOME TO BAMAZON, VALUED* CUSTOMER!`);
+  connection.query('SELECT * FROM products', function(err, data) {
+    if (err) throw err;
+
+    cLog(`==========CURRENT INVENTORY==========`);
+    for (var i = 0; i < data.length; i++){
+      cLog(`SKU#: ${data[i].item_id} || Product: ${data[i].product_name} || Department: ${data[i].department_name} || Price: $${data[i].price} || In Stock: ${data[i].stock_quantity}`)
+    }
+    cLog(`==========^ ^ ^ ^ ^ ^ ^ ^============`);
+
+  })
+
+}
+
+fullInventory();
