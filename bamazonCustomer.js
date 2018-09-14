b98b73ad91be3421b5a9ea22ffbf061cf602a0d0
Andrew Harding <dreweharding@gmail.com>
2018-09-14 00:43:47 -0400

    inquirer fires at the right line

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,5 @@
-var cLog = console.log;
-
 var inquirer = require('inquirer');
 var mysql = require('mysql');
-
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -10,27 +7,42 @@ var connection = mysql.createConnection({
   password: "SuperSecretPasswordHere",
   database: "bamazon"
 });
+var cLog = console.log;
 
-function purchaseInquire() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "sku",
-      message: "What is the SKU number of the item you would like to purchase?",
-    },
-    {
-
-    }
-  ]).then(function (input) {
-
-  })
-
-}
-
-function fullInventory() {
+function startBamazon() {
   cLog(`WELCOME TO BAMAZON, VALUED* CUSTOMER!`);
   connection.query('SELECT * FROM products', function (err, data) {
     if (err) throw err;
+
+
+    function validation(value) {
+      if (!isNaN(value) && parseInt(value) > 0 && parseInt(value) < data.length) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+    function purchaseInquire(x) {
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "sku",
+          message: "What is the SKU number of the item you would like to purchase?",
+          validate: validation
+        },
+        {
+          type: "input",
+          name: "quantity",
+          message: "How many would you like?",
+          validate: validation
+        }
+      ]).then(function (input) {
+        cLog(input.sku);
+        cLog(input.quantity);
+      })
+    }
 
     cLog(`==========CURRENT INVENTORY==========`);
     data.forEach(element => {
@@ -38,10 +50,9 @@ function fullInventory() {
     });
     cLog(`==========^ ^ ^ ^ ^ ^ ^ ^============`);
 
+    purchaseInquire();
+
   })
-
-  purchaseInquire();
-
 }
 
-fullInventory();
+startBamazon();
